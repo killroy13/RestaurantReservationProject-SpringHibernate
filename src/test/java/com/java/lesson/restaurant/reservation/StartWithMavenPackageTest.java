ed971060178a2259867ddb02b901597cba64e7b4
killroy13 <hate66613@mail.ru>
2018-03-27 16:41:11 +0300

    create tests

---
+++ src/test/java/com/java/lesson/restaurant/reservation/StartWithMavenPackageTest.java
@@ -0,0 +1,36 @@
+//package com.java.lesson.restaurant.reservation;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.util.ArrayList;
+//
+///**
+// * Created by User on 23.03.2018.
+// */
+//public class StartWithMavenPackageTest {
+//    @Before
+//    public void setUp() throws Exception {
+//        ArrayList<String> list = new ArrayList<>();
+//    }
+//
+//    @After
+//    public void startAfterTest() throws Exception {
+//        System.out.println("After Test ");
+//    }
+//
+//    @Test
+//    public void startFirstTest() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void startSecondTest() throws Exception {
+//        System.out.println("Second TEst");
+//    }
+//
+//
+//
+//
+//}
