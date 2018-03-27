23143f46e54c71fca7916583e2057953f5a3f456
killroy13 <hate66613@mail.ru>
2018-03-27 16:41:11 +0300

    create tests

---
+++ src/test/java/com/java/lesson/restaurant/reservation/dao/AbstractMySQLDaoTest.java
@@ -0,0 +1,22 @@
+//package com.java.lesson.restaurant.reservation.dao;
+//
+//import org.dbunit.DBTestCase;
+//import org.dbunit.dataset.IDataSet;
+//import org.dbunit.operation.DatabaseOperation;
+//
+///**
+// * Created by User on 24.03.2018.
+// * @author Igor Iv.
+// */
+//public class AbstractMySQLDaoTest extends DBTestCase {
+//
+//    @Override
+//    protected IDataSet getDataSet() throws Exception {
+//        return null;
+//    }
+//
+//    @Override
+//    protected DatabaseOperation getTearDownOperation() throws Exception {
+//        return DatabaseOperation.DELETE_ALL;
+//    }
+//}
